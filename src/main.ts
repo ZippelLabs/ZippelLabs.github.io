e402e4ebca43cde895f149b57519bd56aae8d1aa
this-vishalsingh <this-vishalsingh@users.noreply.github.com>
2025-12-13 17:38:20 +0530

    Initial scaffold with Vite + Tailwind

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import './style.css'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div class="test-container">
+    <h1 class="text-4xl font-bold text-center mt-20 text-white">ZippelLabs ZP1</h1>
+    <p class="text-center text-gray-400">The Limitless M31 zkVM</p>
+  </div>
+`
