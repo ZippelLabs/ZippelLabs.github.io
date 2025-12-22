2c10d5909700e6be96f6d712668bd693356e6427
this-vishalsingh <this-vishalsingh@users.noreply.github.com>
2025-12-22 15:48:30 +0530

    chore: change button text to 'Book an audit'

--- src/main.ts
+++ src/main.ts
@@ -238,7 +238,7 @@ app.innerHTML = `
         </svg>
       </a>
       <a href="/audits.html" class="btn-secondary">
-        Request Audit
+        Book an audit
         <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
           <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"/>
         </svg>
